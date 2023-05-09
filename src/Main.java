fb3d3e23f68bf6a13d717cd4024abdedcbb741df
visareo <130570639+visareo@users.noreply.github.com>
2023-05-10 02:17:10 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int amount = 999; //кол-во денег на счету
+        int amount = 100; //кол-во денег на счету
         System.out.println("На счету " + amount + " рублей");
 
-        int deposit = 1001; //кол-во денег внесенное пользователем для пополнения счета
+        int deposit = 1100; //кол-во денег внесенное пользователем для пополнения счета
         int Bonus; //кол-во бонусов
         int TotalSum; //сумма счета вместе с бонусами
         System.out.println("Сумма пополнения: " + deposit);
 
-        if (deposit < 1001) {
+        if (deposit < 1100) {
             TotalSum = amount + deposit; //Сохранение в переменную TotalSum итоговой суммы счета вместе с бонусами
             Bonus = 0; //Сохранение в переменную Bonus кол-ва бонусов
             System.out.println("Начислено " + Bonus + " бонусов.");
